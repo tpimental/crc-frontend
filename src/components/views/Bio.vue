8c4044489d81cedbcefde784eb38964000c3c3f3
Tyler Pimental <tylerpimental@gmail.com>
2023-11-08 13:20:28 -0800

    About complete + site counter

--- src/components/views/Bio.vue
+++ src/components/views/Bio.vue
@@ -5,7 +5,8 @@
     <h4 class="subtitle" style="padding-bottom: 10px">
         Systems Administration | Cyber Security | Cloud Technologies
     </h4>
-    <h4 class="subtitle">Singapore 📍</h4>
+    <h4 class="subtitle">Twentynine Palms, CA 📍</h4>
+    <span class="subtitle" id="visitor-count"></span>
     <div class="badge-grid">
       <img
         class="badge"
@@ -26,186 +27,24 @@
     <div class="inner-content">
       <h3 class="heading"><b>About Me</b></h3>
       <p class="paragraph">
-        Hello there! I am Don from Singapore and I am an 18 year old student
-        who's extremely passionate in both programming and design for both the
-        web and iOS platform. I am currently still a student in high school but
-        let not my age be the key defining factor as I often aim to punch above
-        my weight.
-      </p>
-      <p class="paragraph">
-        I have worked with a mixture of design and frontend/backend
-        technologies, be it for Web Development or iOS Development.
-      </p>
-    </div>
-    <div class="inner-content">
-      <h3 class="heading"><b>NOSSAFLEX</b></h3>
-      <p class="paragraph">
-        NOSSAFLEX is an EXIF organising tool for analog photographers like
-        myself. It was built using Swift along with the newest framework from
-        Apple, SwiftUI and also a NoSQL database from Google, Firebase. It is an
-        organisation based and registered in Australia.
-      </p>
-      <p class="paragraph">
-        Team NOSSAFLEX consists of a small but tightly knit team. They are,
-        Diego from the USA, Josh and his sister, Maddie from Australia and
-        myself from Singapore. We are an international team that complements
-        each other's strengths. Josh being the brains behind this operations
-        leads our team in marketting as well as the UI/UX design of the app,
-        Diego leads the team with his prowness in Swift as a backend language
-        and I lead the team in the frontend department, helping assemble Josh's
-        ideas into reality using SwiftUI.
-      </p>
-      <p class="paragraph">
-        We recently launched our app onto the international App Store,
-        exclusively for iOS and we are proud to see the support the Analog
-        community has given us. Over 1k new and unique users signed up for the
-        app within the first weeks of the launch, not too shabby considering how
-        niche our target group is.
+        I am currently a Cloud Specialist dedicated to optimizing 
+        system performance and driving innovation. Passionate about 
+        leveraging cloud technologies to enhance efficiency and scalability. 
+        Skilled in troubleshooting complex issues and providing expert solutions. 
+        Constantly seeking opportunities to expand knowledge and stay at the forefront 
+        of emerging trends. Adept at collaborating with cross-functional teams, 
+        utilizing effective communication to foster seamless integration and achieve
+        organizational goals. Former United States Marine Corps service has instilled 
+        discipline, adaptability, and a relentless drive for excellence.
       </p>
     </div>
-
-    <div class="inner-content">
-      <h3 class="heading"><b>Apple WWDC Scholar 2021</b></h3>
-      <p class="paragraph">
-        My second try at Apple's international coding competition was finally a
-        successful one. Making use of my prowess with web development, I decided
-        to create a Swift Playground project that taught users how to make use
-        of HTML and CSS to create a simple website.
-      </p>
-      <p class="paragraph">
-        Making use of Apple's SwiftUI framework as well as the nifty WKWebView
-        API, I managed to create a working editor that would allow users to
-        meddle with HTML and CSS. I proceeded to create a playground book
-        project around that idea and came up with a tutorial sequence for the
-        user to follow.
-      </p>
-      <p class="paragraph">
-        Check out my submission
-        <a style="color: #ff5733" href="https://github.com/DonChiaQE/WWDC2021"
-          >here</a
-        >!
-      </p>
-      <p class="paragraph">
-        And check out my interview with the local news outlet regarding
-        NOSSAFLEX and my WWDC experience
-        <a
-          style="color: #ff5733"
-          href="https://www.youtube.com/watch?v=RwtzXxSSJAc"
-          >right here</a
-        >!
-      </p>
-    </div>
-
-    <div class="inner-content">
-      <h3 class="heading"><b>Apple WWDC Scholar 2022</b></h3>
-      <p class="paragraph">
-        Yet another successful attempt at Apple's international coding
-        competition. By making use of SwiftUI once more, I created a short app
-        experience that explores recursive art patterns and abstract art. With
-        the help of generative art, I was able to create a unique experience
-        that most people would have rarely come across.
-      </p>
-      <p class="paragraph">
-        Check out my submission
-        <a style="color: #ff5733" href="https://github.com/DonChiaQE/ReGen"
-          >here</a
-        >!
-      </p>
-      <p class="paragraph">
-        As this news has only just recently reached me, I will be updating this
-        section once more when I have receive more news from Apple HQ.
-      </p>
-    </div>
-
-    <div class="inner-content">
-      <h3 class="heading"><b>Internships</b></h3>
-      <p class="paragraph">
-        I have had two internship opportunities so far. The first internship was
-        at a local SME firm that created a variety of software technologies for
-        projects and the second internship was a local startup Fintech firm.
-      </p>
-      <p class="paragraph">
-        Throughout the internships I worked with a mixture of iOS and Web
-        Technologies. Storyboarding and UIKit was used in my first tech
-        internship and Vuejs with Bootstrap was used in my second internship as
-        I was a frontend intern.
-      </p>
-    </div>
-
-    <div class="inner-content">
-      <h3 class="heading"><b>iOS Developer</b></h3>
-      <p class="paragraph">
-        As listed above, I have over a year of experience with SwiftUI and have
-        had plenty of projects that features the use of SwiftUI. SwiftUI was
-        used in most of the components building NOSSAFLEX and the components
-        featured required custom styling to fit our themeing. I have also had
-        the chance to participate in two WWDC Student Challenges in 2020 as well
-        as 2021, both projects featured SwiftUI and a variety of other Swift
-        technologies.
-      </p>
-      <p class="paragraph">
-        Here are some of the projects I have contributed towards.
-        <a style="color: #ff5733" href="https://github.com/DonChiaQE/WWDC2021"
-          >[Submission for WWDC 2021]</a
-        >
-        <a
-          style="color: #ff5733"
-          href="https://github.com/DonChiaQE/push-pop-push-views-swiftui-example"
-          >[Push and Popping Solution for SwiftUI]</a
-        >
-      </p>
-    </div>
-
-    <div class="inner-content">
-      <h3 class="heading"><b>Web Developer</b></h3>
-      <p class="paragraph">
-        As a web developer, I have also had a lot of chances to practice and
-        participate in projects to further hone my skills. I have plenty of
-        experience with backend Python as well as databases using SQL or NoSQL
-        solutions. But most of my time is dedicated to frontend development
-        using Vuejs and elaborate styling of websites (such as this one 😁).
-      </p>
-      <p class="paragraph">
-        Here are some of the projects I have contributed towards.
-        <a
-          style="color: #ff5733"
-          href="https://github.com/dunmanhigh-isac/dhap-2021"
-          >[MUN Website for School]</a
-        >
-        <a
-          style="color: #ff5733"
-          href="https://github.com/DonChiaQE/marketplace"
-          >[CMS Marketplace]</a
-        >
-        <a style="color: #ff5733" href="https://github.com/zaydek/duomo"
-          >[SCSS Library]</a
-        >
-      </p>
-    </div>
-
-    <div class="inner-content">
-      <h3 class="heading"><b>UI/UX Designer</b></h3>
-      <p class="paragraph">
-        Fullstack development is very sought after nowadays and I have made sure
-        to keep up with such trends by honing my skills in UI/UX design as well.
-        I am thankful for the various opportunities I have had to work alongside
-        industry expert and have also taken the chance to learn and hone my
-        UI/UX design skills with tools such as Figma.
-      </p>
-    </div>
-
-    <!-- <div class="inner-content">
-                            <div class="heading"><b>Interests and Hobbies</b></div>
-                            <div class="paragraph">Analog photography and colorizing of historical photos have always interested me and some of my photographic works are displayed <span style="cursor: pointer; color: #FF5733;" v-on:click="showPhotos">here</span> while you can find my colorization portfolio <span style="cursor: pointer; color: #FF5733;" v-on:click="showColorization">here</span>.</div>
-                        </div> -->
-
     <div class="inner-content">
       <h3 class="heading"><b>Résumé</b></h3>
       <a
         style="color: #ff5733"
-        href="/files/don_chia_resume.pdf"
+        href="/files/tyler_pimental_resume.pdf"
         class="paragraph"
-        >Check out my résumé here.</a
+        >Check out my resume here.</a
       >
     </div>
   </div>
@@ -258,3 +97,70 @@ h3 {
   margin: 0;
 }
 </style>
+
+<script>
+const outputElement = document.getElementById("visitor-count");
+
+window.onload = () => {
+    //webpage is loaded
+    //increment the counter
+    fetch("https://tpimental-apimgmt.azure-api.net/backend-functionapp-current/increment-count")
+        .then(response => {
+
+        })
+        .catch(error => {
+            console.log("Increment count error:", error);
+
+        })
+    //get the new count
+    fetch("https://tpimental-apimgmt.azure-api.net/backend-functionapp-current/get-count")
+        .then(response => {
+            if(response.ok){
+                return response.json();
+            }else {
+                return response.json().then(data => {
+                    throw new Error(data.message); // If not successful, throw an error with the message
+                });
+            }
+        })
+        .then(data => {
+            //Process the data from the API response
+            const responseData = data;
+
+            outputElement.innerHTML = addNumberSuffix(data);
+            console.log(responseData);
+        })
+        .catch(error => {
+            // Error handling
+            console.error("Get Count Error: ", error);
+        });
+}
+
+function addNumberSuffix(number) {
+    if (typeof number !== 'number' || isNaN(number)) {
+        return 'Invalid input';
+    }
+
+    const lastDigit = number % 10;
+    const secondLastDigit = Math.floor((number % 100) / 10);
+
+    if (secondLastDigit === 1) {
+        return number + 'th';
+    } else {
+        switch (lastDigit) {
+            case 1:
+                return number + 'st';
+            case 2:
+                return number + 'nd';
+            case 3:
+                return number + 'rd';
+            default:
+                return number + 'th';
+        }
+    }
+}
+
+
+
+
+</script>
