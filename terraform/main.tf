00cb675345b36752b20cb727151dbc244bbd526a
tpimental <tpimental@expansiagroup.com>
2026-02-12 17:16:55 -0500

    clean up

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,17 @@
 locals {
-
-
+  domain_name = "tpimental.com"
+  origin_id   = "s3-site-origin"
 }
 
-// S3
-resource "aws_s3_bucket" "tpimental-crc" {
-    bucket = "tpimental-crc"
+# --------------------
+# S3 Bucket (Private)
+# --------------------
+resource "aws_s3_bucket" "site" {
+  bucket = "tpimental-crc"
 }
 
-resource "aws_s3_bucket_ownership_controls" "tpimental-crc-ctrl" {
-  bucket = aws_s3_bucket.tpimental-crc.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "tpimental-crc-acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.tpimental-crc-ctrl]
-
-  bucket = aws_s3_bucket.tpimental-crc.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_public_access_block" "block" {
-  bucket = aws_s3_bucket.tpimental-crc.id
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket = aws_s3_bucket.site.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -31,71 +19,44 @@ resource "aws_s3_bucket_public_access_block" "block" {
   restrict_public_buckets = true
 }
 
-// Cloudfront distribution
-
-# See https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-restricting-access-to-s3.html
-data "aws_iam_policy_document" "origin_bucket_policy" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
-
-    actions = ["s3:GetObject"]
-
-    resources = [
-      "${aws_s3_bucket.tpimental-crc.arn}/*"
-    ]
-
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceArn"
-      values   = [aws_cloudfront_distribution.site.arn]
-    }
-  }
-}
-
-
-resource "aws_s3_bucket_policy" "tpimental-crc-policy" {
-  bucket = aws_s3_bucket.tpimental-crc.bucket
-  policy = data.aws_iam_policy_document.origin_bucket_policy.json
-}
-
-locals {
-  s3_origin_id = "myS3Origin"
-  my_domain    = "tpimental.com"
-}
-
-data "aws_acm_certificate" "my_domain" {
-  region   = "us-east-1"
-  domain   = "*.${local.my_domain}"
-  statuses = ["ISSUED"]
-}
-
-resource "aws_cloudfront_origin_access_control" "default" {
-  name                              = "default-oac"
+# --------------------
+# CloudFront OAC
+# --------------------
+resource "aws_cloudfront_origin_access_control" "site" {
+  name                              = "tpimental-oac"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
 }
 
+# --------------------
+# ACM Certificate (must be in us-east-1)
+# --------------------
+data "aws_acm_certificate" "site" {
+  provider = aws.us_east_1
+  domain   = local.domain_name
+  statuses = ["ISSUED"]
+}
+
+# --------------------
+# CloudFront Distribution
+# --------------------
 resource "aws_cloudfront_distribution" "site" {
+
   origin {
-    domain_name              = aws_s3_bucket.tpimental-crc.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
-    origin_id                = local.s3_origin_id
+    domain_name              = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.site.id
+    origin_id                = local.origin_id
   }
 
   enabled             = true
   is_ipv6_enabled     = true
   default_root_object = "index.html"
 
-  aliases = ["${local.my_domain}"]
+  aliases = [local.domain_name]
 
   default_cache_behavior {
-    target_origin_id       = local.s3_origin_id
+    target_origin_id       = local.origin_id
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
     viewer_protocol_policy = "redirect-to-https"
@@ -120,40 +81,62 @@ resource "aws_cloudfront_distribution" "site" {
     response_page_path = "/index.html"
   }
 
-  viewer_certificate {
-    acm_certificate_arn = data.aws_acm_certificate.my_domain.arn
-    ssl_support_method  = "sni-only"
-  }
-
   restrictions {
     geo_restriction {
       restriction_type = "none"
     }
   }
+
+  viewer_certificate {
+    acm_certificate_arn = data.aws_acm_certificate.site.arn
+    ssl_support_method  = "sni-only"
+  }
 }
 
+# --------------------
+# S3 Bucket Policy (Allow CF Only)
+# --------------------
+data "aws_iam_policy_document" "site_bucket_policy" {
+  statement {
+    effect = "Allow"
 
-# Create Route53 records for the CloudFront distribution aliases
-data "aws_route53_zone" "my_domain" {
-  name = local.my_domain
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = ["s3:GetObject"]
+
+    resources = ["${aws_s3_bucket.site.arn}/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.site.arn]
+    }
+  }
 }
 
-resource "aws_route53_record" "cloudfront" {
-  for_each = aws_cloudfront_distribution.s3_distribution.aliases
-  zone_id  = data.aws_route53_zone.my_domain.zone_id
-  name     = each.value
-  type     = "A"
+resource "aws_s3_bucket_policy" "site" {
+  bucket = aws_s3_bucket.site.id
+  policy = data.aws_iam_policy_document.site_bucket_policy.json
+}
+
+# --------------------
+# Route53
+# --------------------
+data "aws_route53_zone" "site" {
+  name = local.domain_name
+}
+
+resource "aws_route53_record" "site" {
+  zone_id = data.aws_route53_zone.site.zone_id
+  name    = local.domain_name
+  type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.site.domain_name
+    zone_id                = aws_cloudfront_distribution.site.hosted_zone_id
     evaluate_target_health = false
   }
 }
-// TLS 
-
-// DNS
-
-// Lambda
-
-// DynamoDB for counter
